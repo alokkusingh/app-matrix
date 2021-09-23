c26dce91c4f0a3a34abff035674b0c525622d924
alokkusingh <alok.ku.singh@gmail.com>
2021-09-23 16:55:36 +0530

    updated k8s yaml

--- src/main/java/com/alok/app/matrix/controller/HelloController.java
+++ src/main/java/com/alok/app/matrix/controller/HelloController.java
@@ -1,17 +1,22 @@
 package com.alok.app.matrix.controller;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/app/hello")
 public class HelloController {
 
+    public static final UUID uuid = UUID.randomUUID();
+
     @Timed(value = "greeting.time", description = "Time taken to return greeting")
     @GetMapping("/greet")
     public String greet() {
-        return "Hello!";
+        return "Hello from " + uuid.toString();
     }
 }
